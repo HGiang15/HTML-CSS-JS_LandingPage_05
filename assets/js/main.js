de80e5c50bfb6b11133ec62fce42a3addb6aa607
HGiang15 <gianghoang150503@gmail.com>
2024-07-15 23:04:14 +0900

    update js

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,32 @@ links.forEach((link) => {
     link.classList.add("navbar__link--active");
   });
 });
+
+// Feedback slider
+document.addEventListener("DOMContentLoaded", function () {
+  const feedbackItems = document.querySelectorAll(".feedback-item");
+  const feedbackCtaItems = document.querySelectorAll(".feedback-cta__item");
+
+  feedbackCtaItems.forEach((ctaItem, index) => {
+    ctaItem.addEventListener("click", () => {
+      feedbackCtaItems.forEach((item) =>
+        item.classList.remove("feedback-cta__item--active")
+      );
+      ctaItem.classList.add("feedback-cta__item--active");
+
+      feedbackItems.forEach((item) => (item.style.display = "none"));
+      const startIndex = index * 2;
+      feedbackItems[startIndex].style.display = "block";
+      if (feedbackItems[startIndex + 1]) {
+        feedbackItems[startIndex + 1].style.display = "block";
+      }
+    });
+  });
+
+  // Hiển thị phần tử đầu tiên khi tải trang
+  feedbackCtaItems[0].classList.add("feedback-cta__item--active");
+  feedbackItems[0].style.display = "block";
+  if (feedbackItems[1]) {
+    feedbackItems[1].style.display = "block";
+  }
+});
